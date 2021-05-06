eec02af7b04942566fb262b94e4d6199b70f2f8a
root <root@NFrag-Home-Desktop.localdomain>
2021-05-05 21:07:47 -0700

    Updates

---
+++ precache-manifest.9a3b118b5e2dc81538928579654991db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ff36698c1c5e1404aeddb09ceb5927",
+    "url": "/React-Markdown_Preview/index.html"
+  },
+  {
+    "revision": "b5cff73486f83014d9d7",
+    "url": "/React-Markdown_Preview/static/css/main.67d641af.chunk.css"
+  },
+  {
+    "revision": "6bbb2575db765f6d65f2",
+    "url": "/React-Markdown_Preview/static/js/2.9d0b99b7.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/React-Markdown_Preview/static/js/2.9d0b99b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5cff73486f83014d9d7",
+    "url": "/React-Markdown_Preview/static/js/main.9906d10e.chunk.js"
+  },
+  {
+    "revision": "328571ea864eb1db8a31",
+    "url": "/React-Markdown_Preview/static/js/runtime-main.afd79b5f.js"
+  }
+]);
